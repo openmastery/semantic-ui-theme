a736a806c223fcda8744134631611c55f123b0bc
Brian Espinosa <b@bje.co>
2017-10-05 11:34:12 -0700

    Fela renderer

---
+++ theme-editor/src/renderer.js
@@ -0,0 +1,37 @@
+import {createRenderer} from 'fela'
+import embedded         from 'fela-plugin-embedded'
+import prefixer         from 'fela-plugin-prefixer'
+import fallbackValue    from 'fela-plugin-fallback-value'
+import unit             from 'fela-plugin-unit'
+import lvha             from 'fela-plugin-lvha'
+import validator        from 'fela-plugin-validator'
+import logger           from 'fela-plugin-logger'
+import perf             from 'fela-perf'
+import beautifier       from 'fela-beautifier'
+
+export default () => {
+  const renderer = createRenderer({
+    plugins: [
+      embedded(),
+      prefixer(),
+      fallbackValue(),
+      unit(),
+      lvha(),
+      validator(),
+      logger()
+    ],
+    enhancers: [perf(), beautifier()]
+  })
+
+  renderer.renderStatic(
+    {
+      width: '100%',
+      height: '100%',
+      margin: 0,
+      padding: 0,
+    },
+    '#root,.sui-theme-editor'
+  )
+
+  return renderer
+}
