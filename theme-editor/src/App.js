6b46090cedea14fdfa1396f2b9be1d858bd51e70
Brian Espinosa <b@bje.co>
2017-10-04 21:09:22 -0500

    Clean up noise in CRA baseline

--- theme-editor/src/App.js
+++ theme-editor/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo                 from './logo.svg';
-import './App.css';
+import {
+  Header,
+  Message
+}                           from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="sui-theme-editor">
+        <Header content='SUIR Header' />
+        <Message content='Here we go...' />
       </div>
     );
   }
