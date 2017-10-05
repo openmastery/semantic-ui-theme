303d4082bd66c9324eafe9eb4907ae26f404cf7b
Brian Espinosa <b@bje.co>
2017-10-05 11:34:26 -0700

    Provider added for fela

--- theme-editor/src/index.js
+++ theme-editor/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React                 from 'react';
+import ReactDOM              from 'react-dom';
+import {Provider}            from 'react-fela';
+
+import App                   from './App';
+import createRenderer        from './renderer'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderer = createRenderer();
+require('../node_modules/semantic-ui-css/semantic.min.css');
+
+ReactDOM.render(
+  <Provider renderer={renderer}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
